19bdde399d707f2f05f911381b350cf4e37a6629
nikitaloginove <nikita.loginove@yandex.ru>
2025-01-04 21:26:17 +0400

    fix ghpages

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,22 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base:  '/stik/',
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
-})
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    
+    return {
+        base: env.NODE_ENV === 'production' ? '/stik/' : '/',
+        plugins: [
+            vue(),
+            vueDevTools(),
+        ],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url))
+            },
+        },
+    };
+});
