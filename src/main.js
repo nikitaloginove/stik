c81a43a3e4907f5d071028780db8fc6ac749f1fb
nikitaloginove <nikita.loginove@yandex.ru>
2025-01-04 18:57:29 +0400

    Test task

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// import {swiper, swiperSlide} from 'swiper/vue';
+// import swiperCore from "swiper";
+
+// import "swiper/swiper.min.css";
+
+// swiperCore.use([/* default global options */])
+
+createApp(App).mount('#app')
