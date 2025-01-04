ebf932ff3b4fceafb6b28fd03d411184aed4cf31
nikitaloginove <nikita.loginove@yandex.ru>
2025-01-04 21:27:05 +0400

    fix ghpages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
     
