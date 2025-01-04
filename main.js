e24905384b47af04dde7b376240019be8b022a16
nikitaloginove <nikita.loginove@yandex.ru>
2025-01-04 21:16:52 +0400

    fix ghpages

--- main.js
+++ main.js
@@ -3,11 +3,4 @@ import './src/assets/main.css'
 import { createApp } from 'vue'
 import App from './src/App.vue'
 
-// import {swiper, swiperSlide} from 'swiper/vue';
-// import swiperCore from "swiper";
-
-// import "swiper/swiper.min.css";
-
-// swiperCore.use([/* default global options */])
-
 createApp(App).mount('#app')
