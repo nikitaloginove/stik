7dfd179282b179ef70089fe71f245c35eb3f2699
nikitaloginove <nikita.loginove@yandex.ru>
2025-01-09 13:34:14 +0300

    feat swiper

--- vite.config.js
+++ vite.config.js
@@ -4,27 +4,18 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig(() => {
-  const isProduction = process.env.NODE_ENV === 'production';
-  const repoName = '/stik/'; 
+  const isProduction = process.env.NODE_ENV === 'production'
+  const repoName = '/stik/'
   return {
     base: isProduction ? repoName : '/',
-    plugins: [
-      vue(),
-      vueDevTools(),
-    ],
+    plugins: [vue(), vueDevTools()],
     resolve: {
       alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
-     build: {
-       outDir: 'docs',
-       rollupOptions:{
-        external: ['swiper']
-        }
-      },
-      optimizeDeps:{
-        include:['swiper']
-      }
-  };
-});
+    build: {
+      outDir: 'docs',
+    },
+  }
+})
